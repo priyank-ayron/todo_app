507521f941ea6a5f97adc162627524c5b1a2fa12
Priyank <93072pri@gmail.com>
2020-08-08 18:53:36 +0530

    adding basic html

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <div className="main_div">
+        <div className="center_div">
+          <br />
+          <h1>TO DO LIST</h1>
+          <br />
+          <input type="text" placeholder="Add an item"></input>
+          <button>+</button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default App;
