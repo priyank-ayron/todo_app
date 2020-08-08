ec1771ba6800eea15e35104a0e5f2b7e5299d2c1
Priyank <93072pri@gmail.com>
2020-08-08 21:55:09 +0530

    updating code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import ToDoLists from "./ToDoLists";
 
 const App = () => {
+  const [inputList, setInputList] = useState("");
+  const [Items, setItemsList] = useState([]);
+
+  const itemEvent = (event) => {
+    setInputList(event.target.value);
+  };
+  const listOfItems = () => {
+    setItemsList((oldItems) => {
+      return [...oldItems, inputList];
+    });
+    setInputList("");
+  };
   return (
     <>
       <div className="main_div">
@@ -8,8 +21,18 @@ const App = () => {
           <br />
           <h1>TO DO LIST</h1>
           <br />
-          <input type="text" placeholder="Add an item"></input>
-          <button>+</button>
+          <input
+            type="text"
+            placeholder="Add an item"
+            value={inputList}
+            onChange={itemEvent}
+          ></input>
+          <button onClick={listOfItems}>+</button>
+          <ol>
+            {Items.map((itemval) => {
+              return <ToDoLists text={itemval} />;
+            })}
+          </ol>
         </div>
       </div>
     </>
