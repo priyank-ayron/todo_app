89bfc326245403204880230843a6b2fa02f511c3
Priyank <93072pri@gmail.com>
2020-08-08 21:54:40 +0530

    todolist component

---
+++ src/ToDoLists.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const ToDoLists = (props) => {
+  return (
+    <>
+      <div className="todo_style">
+        <i class="fa fa-times" aria-hidden="true"></i>
+        <li>{props.text}</li>
+      </div>
+    </>
+  );
+};
+
+export default ToDoLists;
