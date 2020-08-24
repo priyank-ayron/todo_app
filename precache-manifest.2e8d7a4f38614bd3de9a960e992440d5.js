10cd6192cd3f5ce423657d32a883806e8192dc04
Priyank Agarwal <priyank.ayron@gmail.com>
2020-08-24 11:05:22 +0530

    Updates

---
+++ precache-manifest.2e8d7a4f38614bd3de9a960e992440d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4da875f847767842eaaef9d5f33c64cb",
+    "url": "/todo_app/index.html"
+  },
+  {
+    "revision": "7679967464b35e24d66c",
+    "url": "/todo_app/static/css/main.15195531.chunk.css"
+  },
+  {
+    "revision": "6f4e09c67d8c48a79763",
+    "url": "/todo_app/static/js/2.5f901d3e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo_app/static/js/2.5f901d3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7679967464b35e24d66c",
+    "url": "/todo_app/static/js/main.2ef895db.chunk.js"
+  },
+  {
+    "revision": "ced6bf582c0630731099",
+    "url": "/todo_app/static/js/runtime-main.45d19e70.js"
+  }
+]);
